e91b6448cf615c6a7ea3ccfb636784c58d6939d2
agoodson582 <agoodson582@gmail.com>
2022-07-25 15:03:51 -0400

    Started adding functionality to Submit button

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ var wordToGuess;
 
 // Element references
 let submitButton = document.getElementById('submit');
-let selectedWord = document.querySelector('.selected-word');
+let selectedWordArea = document.querySelector('.selected-word');
+let guessHistoryPanel = document.querySelector('.guess-history');
+let guessList = document.querySelector('.guess-list');
 
 // Event listeners
 submitButton.addEventListener("click", onSubmitClick);
@@ -24,10 +26,22 @@ function setupGame() {
 
     // Select a random word for the player to guess
     wordToGuess = words[Math.floor(Math.random() * words.length)];
-    selectedWord.innerHTML = '_'.repeat(wordToGuess.length);
+    selectedWordArea.innerHTML = '_'.repeat(wordToGuess.length);
+
+    console.log(`Word to guess: ${wordToGuess}`);
 }
 
 // Input event listeners
 function onSubmitClick() {
     let guess = document.getElementById('guess-textbox').value;
+    console.log(`guess: ${guess}`);
+
+    // Make sure guess-history div is visible first
+    guessHistoryPanel.style.visibility = 'visible';
+    
+    var guessItem = document.createElement('li');
+    guessItem.append(guess);
+
+    guessList.appendChild(guessItem);
+
 }
